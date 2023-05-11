a827773be172c71e6221e8d5ad0ff418798697ce
Deven Umrania <umraniadeven10@gmail.com>
2023-05-12 00:59:54 +0530

    Update: Added Pages, Login & Registration forms

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+// libraries
 import { Routes, Route } from "react-router-dom";
+
+// component / pages
 import { Navbar } from "./components/Navbar/Navbar";
 import { Home } from "./pages/Home/Home";
 import { Shop } from "./pages/Shop/Shop";
 import { Cart } from "./pages/Cart/Cart";
 import { Wishlist } from "./pages/Wishlist/Wishlist";
 import { SingleProduct } from "./pages/Shop/SingleProduct";
+import { Profile } from "./pages/Profile/Profile";
+import { Login } from "./pages/Login/Login";
+import { Register } from "./pages/Register/Register";
+
+// styling
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +25,9 @@ function App() {
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/shop/:productId" element={<SingleProduct />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,17 @@
+// libraries
+import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
+import { Tooltip } from "react-tooltip";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+// components
+
+// styling
 import "./Navbar.css";
+import "react-tooltip/dist/react-tooltip.css";
 
 const Logo = () => {
   return (
@@ -10,21 +22,105 @@ const Logo = () => {
 };
 
 const Nav = () => {
+  const [loginToken, setLoginToken] = useState(false);
+
+  const ThreeDots = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    return (
+      <>
+        <div>
+          <IconButton
+            className="more-options"
+            aria-label="more"
+            id="long-button"
+            aria-controls={open ? "long-menu" : undefined}
+            aria-expanded={open ? "true" : undefined}
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            <MoreVertIcon />
+          </IconButton>
+          <Menu
+            id="long-menu"
+            MenuListProps={{
+              "aria-labelledby": "long-button",
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            // PaperProps={{
+            //   style: {
+            //     width: "",
+            //   },
+            // }}
+          >
+            <MenuItem onClick={handleClose}>
+              <Link className="profile-link" to="/profile">
+                Profile
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <button className="logout-button">Logout</button>
+            </MenuItem>
+          </Menu>
+        </div>
+      </>
+    );
+  };
+
   return (
     <>
       <div className="nav-container">
-        <NavLink className="navLink" to="/">
-          Home
-        </NavLink>
-        <NavLink className="navLink" to="/shop">
-          Shop
-        </NavLink>
-        <NavLink className="navLink" to="/cart">
-          Cart
-        </NavLink>
-        <NavLink className="navLink" to="/wishlist">
-          Wishlist
-        </NavLink>
+        <div className="nav-links">
+          <Tooltip id="Cart-tooltip" />
+          <NavLink
+            data-tooltip-id="Cart-tooltip"
+            data-tooltip-content="Shop"
+            data-tooltip-place="bottom"
+            className="navLink"
+            to="/cart"
+          >
+            <i className="fa-solid fa-cart-shopping"></i>
+          </NavLink>
+
+          <Tooltip id="Wishlist-tooltip" />
+          <NavLink
+            data-tooltip-id="Wishlist-tooltip"
+            data-tooltip-content="Wishlist"
+            data-tooltip-place="bottom"
+            className="navLink"
+            to="/wishlist"
+          >
+            <i className="fa-regular fa-heart"></i>
+          </NavLink>
+        </div>
+
+        <div>
+          {loginToken ? (
+            <ThreeDots />
+          ) : (
+            <>
+              <Tooltip id="Login-tooltip" />
+              <NavLink
+                data-tooltip-id="Login-tooltip"
+                data-tooltip-content="Login"
+                data-tooltip-place="bottom"
+                className="navLink"
+                to="/login"
+              >
+                <i className="fa-regular fa-user"></i>
+              </NavLink>
+            </>
+          )}
+        </div>
       </div>
     </>
   );
